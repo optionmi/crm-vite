26f6135a372b11e1e0eca369f26f9e86e1b21c1f
Md-Aquib <aquibali1209@gmail.com>
2023-09-16 16:34:00 +0530

    'create_publisher'

--- src/api/publisherAPI.js
+++ src/api/publisherAPI.js
@@ -4,18 +4,25 @@ const BASE_URL = "http://localhost:8000"; // Replace with your backend API URL
 
 const publishersApi = {
     // Create a new publisher
-    createPublisher: async (e, authToken) => {
+    createPublisher: async (publisherData, authToken) => {
         try {
             let response = await fetch(`${BASE_URL}/api/publisher/`, {
-                method: "GET",
+                method: "POST",
                 headers: {
                     Authorization: `Bearer ${authToken}`,
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                    company_name: e.target.company_name.value,
-                    email: e.target.email.value,
-                    password: e.target.password.value,
+                    company_name: publisherData.company_name,
+                    email: publisherData.email,
+                    country: publisherData.country,
+                    state: publisherData.state,
+                    city: publisherData.city,
+                    address: publisherData.address,
+                    contact_person:publisherData.contact_person,
+                    postal_code: parseInt(publisherData.postal_code),
+                    phone_number: parseInt(publisherData.phone_number),
+                    password:publisherData.password,
                 }),
             });
             return response.data;
--- src/pages/create/CreatePublisher.js
+++ src/pages/create/CreatePublisher.js
@@ -1,131 +1,234 @@
-import React from 'react';
+import React, { useState, useContext} from 'react';
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
-import { Card, Form, Button, Container, Row} from 'react-bootstrap';
+import { Card, Form, Button} from 'react-bootstrap';
 import publishersApi from '../../api/publisherAPI';
+import { Country, State, City }  from 'country-state-city';
+import AuthContext from '../../context/AuthContext';
+import {useNavigate } from 'react-router-dom';
 
 function CreatePublisher() {
-  // const handleCreatePublisher = (e) => {
-  //   e.preventDefault();
-  //   const publisherData = {
-  //     name: e.target.name.value,
-  //     address: e.target.address.value,
-  //     contactInfo: e.target.contact.value,
-  //   };
-  //   publishersApi
-  //     .createPublisher(publisherData)
-  //     .then((createdPublisher) => {
-  //       setPublishers((prevPublishers) => [...prevPublishers, createdPublisher]);
-  //       setNewPublisher({
-  //         name: '',
-  //         address: '',
-  //         contactInfo: '',
-  //       });
-  //       handleClose();
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error creating publisher:', error);
-  //     });
-  // };
-  const countries = [
-    { code: 'us', name: 'United States' },
-    { code: 'ca', name: 'Canada' },
-  ];
+
+  let {authToken} = useContext(AuthContext)
+
+  const navigate = useNavigate()
+
+  const [selectedCountry, setSelectedCountry] = useState('')
+  const [selectedState, setSelectedState] = useState('')
+  const [selectedCity, setSelectedCity] = useState('')
+
+  const countries = Country.getAllCountries();
+  const states = selectedCountry ? State.getStatesOfCountry(selectedCountry): [];
+  const cities = selectedState ? City.getCitiesOfState(selectedCountry,selectedState) : [];
+
+
+  const handleCountryChange = (e) => {
+    setSelectedCountry(e.target.value);
+  };
+
+  const handleStateChange = (e) => {
+    setSelectedState(e.target.value);
+  };
+
+  const handleCityChange = (e) => {
+    setSelectedCity(e.target.value);
+  };
+
+  const handleCreatePublisher = (e) => {
+
+    const password = e.target.password.value;
+    const confirm_password = e.target.confirm_password.value;
+
+    if(password===confirm_password){
+      e.preventDefault();
+      const publisherData = {
+        name: e.target.company_name.value,
+        country: Country.getCountryByCode(selectedCountry).name,
+        email:e.target.email.value,
+        state: State.getStateByCode(selectedState).name,
+        address:e.target.address.value,
+        city: selectedCity,
+        contact_person: e.target.contact_person.value,
+        postal_code: e.target.postal_code.value,
+        phone_number: e.target.phone_number.value,
+        password: password
+      };
+      publishersApi
+        .createPublisher(publisherData, authToken)
+        .then(() => {
+          console.log('log')
+          // navigate('/publishers')
+        })
+        .catch((error) => {
+          console.error('Error creating publisher:', error);
+        });
+      } else{
+        console.error('password not matched !')
+      }
+  };
+
   return (
     <div>
         <Header/>
         <Sidebar/>
         <div className="publisher">
           <h4>Create Publisher</h4>
-          <Container>
-            <Row>
-                    <Card className="create-publisher-card shadow-sm" style={{'background': 'white'}} >
-                      <Card.Body>
-                        <Form className='create-publisher-form'>
-                          <Form.Group controlId="company-name" className='create-publisher-form-group'>
-                            <Form.Label>Company Name</Form.Label>
-                            <Form.Control className='form-contol'
-                            type="text"
-                            placeholder="Company Name"
-                            name='company_name'
-                            required
-                            />
-                          </Form.Group>
+          <Card className="create-publisher-card shadow-sm" style={{'background': 'white', 'height': 'fit-content'}} >
+            <Card.Body className='create-publisher-card-body'>
+              <Form className='create-publisher-form' onSubmit={handleCreatePublisher}>
+                <div className="row">
 
-                          <Form.Group controlId="email" className='create-publisher-form-group'>
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control className='form-contol'
-                            type="email"
-                            placeholder="Email"
-                            name='email'
-                            required
-                            />
-                          </Form.Group>
+                  <div className='col-lg-6 col-md-6 col-12'>
+                    <Form.Group controlId="company-name" className='create-publisher-form-group'>
+                      <Form.Label>Company Name</Form.Label>
+                      <Form.Control className='form-contol'
+                      type="text"
+                      placeholder="Company Name"
+                      name='company_name'
+                      required
+                      />
+                    </Form.Group>
+                  </div>
 
-                          <Form.Group controlId="address" className='create-publisher-form-group'>
-                            <Form.Label>Address</Form.Label>
-                            <Form.Control className='form-contol'
-                            type="text"
-                            placeholder="Address"
-                            name='address'
-                            required
-                            />
-                          </Form.Group>
+                  <div className='col-lg-6 col-md-6 col-12'>
+                    <Form.Group>
+                      <Form.Label>Country</Form.Label>
+                      <Form.Control as="select" onChange={handleCountryChange}>
+                        <option value="">Select a country</option>
+                        {countries.map((country) => (
+                        <option key={country.isoCode} value={country.isoCode}>
+                          {country.name}
+                        </option>
+                      ))}
+                      </Form.Control>
+                    </Form.Group>
+                  </div>
 
-                          <Form.Group controlId="contact-person" className='create-publisher-form-group'>
-                            <Form.Label>Contact Person</Form.Label>
-                            <Form.Control className='form-contol'
-                            type="text"
-                            placeholder="Contact Person"
-                            name='contact_person'
-                            required
-                            />
-                          </Form.Group>
+                  <div className='col-lg-6 col-md-6 col-12'>
+                    <Form.Group controlId="email" className='create-publisher-form-group'>
+                      <Form.Label>Email</Form.Label>
+                      <Form.Control className='form-contol'
+                      type="email"
+                      placeholder="Email"
+                      name='email'
+                      required
+                      />
+                    </Form.Group>
+                  </div>
 
-                          <Form.Group controlId="phone-number" className='create-publisher-form-group'>
-                            <Form.Label>Phone Number</Form.Label>
-                            <Form.Control className='form-contol'
-                            type="number"
-                            placeholder="Phone Number"
-                            name='phone_number'
-                            required
-                            />
-                          </Form.Group>
+                  <div className='col-lg-6 col-md-6 col-12'>
+                    <Form.Group>
+                      <Form.Label>State</Form.Label>
+                      <Form.Control as="select" onChange={handleStateChange} disabled={!selectedCountry}>
+                        <option value="">Select a state</option>
+                        {states.map((state) => (
+                          <option key={state.isoCode} value={state.isoCode}>
+                            {state.name}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Form.Group>
+                  </div>
 
-                          <Form.Group controlId="phone-number" className='create-publisher-form-group'>
-                            <Form.Label>Phone Number</Form.Label>
-                            <Form.Control className='form-contol'
-                            type="number"
-                            placeholder="Phone Number"
-                            name='phone_number'
-                            required
-                            />
-                          </Form.Group>
+                  <div className='col-lg-6 col-md-6 col-12'>
+                    <Form.Group controlId="address" className='create-publisher-form-group'>
+                      <Form.Label>Address</Form.Label>
+                      <Form.Control className='form-contol'
+                      type="text"
+                      placeholder="Address"
+                      name='address'
+                      required
+                      />
+                    </Form.Group>
+                  </div>
 
-                          <Form.Group controlId="countrySelect" className='create-publisher-form-group'>
-                            <Form.Label>Select Country</Form.Label>
-                            <Form.Control as="select">
-                              {countries
-                              .map((country) => (
-                              <option key={country.code} value={country.name}>
-                                {country.name}
-                              </option>
-                              ))}
-                            </Form.Control>
-                          </Form.Group>
+                  <div className='col-lg-6 col-md-6 col-12'>
+                    <Form.Group>
+                      <Form.Label>City</Form.Label>
+                      <Form.Control as="select" disabled={!selectedState} onChange={handleCityChange}>
+                      <option value="">Select a city</option>
+                      {cities.map((city) => (
+                        <option key={city.name} value={city.name}>
+                          {city.name}
+                        </option>
+                      ))}
+                      </Form.Control>
+                    </Form.Group>
+                  </div>
 
+                  <div className='col-lg-6 col-md-6 col-12'>
+                    <Form.Group controlId="contact-person" className='create-publisher-form-group'>
+                      <Form.Label>Contact Person</Form.Label>
+                      <Form.Control className='form-contol'
+                      type="text"
+                      placeholder="Contact Person"
+                      name='contact_person'
+                      required
+                      />
+                    </Form.Group>
+                  </div>
 
+                  <div className='col-lg-6 col-md-6 col-12'>
+                    <Form.Group controlId="postal-code" className='create-publisher-form-group'>
+                      <Form.Label>Postal Code</Form.Label>
+                      <Form.Control className='form-contol'
+                      type="number"
+                      placeholder="Postal Code"
+                      name='postal_code'
+                      required
+                      />
+                    </Form.Group>
+                  </div>
 
+                  <div className='col-lg-6 col-md-6 col-12'>
+                    <Form.Group controlId="phone-number" className='create-publisher-form-group'>
+                      <Form.Label>Phone Number</Form.Label>
+                      <Form.Control className='form-contol'
+                      type="number"
+                      placeholder="Phone Number"
+                      name='phone_number'
+                      required
+                      />
+                    </Form.Group>
+                  </div>
 
+                </div>
 
-                                <Button variant="primary" type="submit" className="w-100 login-form-group login-button" >
-                                    Login
-                                </Button>
-                            </Form>
-                        </Card.Body>
-                    </Card>
-            </Row>
-        </Container>
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <Form.Group controlId="password" className='create-publisher-form-group'>
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control className='form-contol'
+                      type="password"
+                      placeholder="Password"
+                      name='password'
+                      required
+                      />
+                    </Form.Group>
+                  </div>
+
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <Form.Group controlId="confirm-password" className='create-publisher-form-group'>
+                      <Form.Label>Confirm Password</Form.Label>
+                      <Form.Control className='form-contol'
+                      type="password"
+                      placeholder="Confirm Password"
+                      name='confirm_password'
+                      required
+                      />
+                    </Form.Group>
+                  </div>
+
+                </div>
+
+                <div className='create-form-btn'>
+                  <Button variant="primary" type="submit" className="w-100 create-publisher-form-group create-publisher-button" >
+                    Create Publisher
+                  </Button>
+                </div>
+              </Form>
+            </Card.Body>
+          </Card>
         </div>
     </div>
   )
