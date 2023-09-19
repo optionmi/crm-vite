a7a3293ace92c4a8a2145de44a82f81a0767afb6
Mohammad Aquib <108718324+Md-Aquib@users.noreply.github.com>
2023-09-19 19:32:10 +0530

    Revert "merge"

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,89 +7,13 @@ faPlane, faFile, faGraduationCap, faBookOpen, faBookAtlas} from '@fortawesome/fr
 
 
 function Header() {
-    let { logoutUser, Name } = useContext(AuthContext);
-    return (
-        <div>
-            <nav className="navbar">
-                <div className="container-fluid d-flex">
-                    <a
-                        className="navbar-brand"
-                        style={{ backgroundColor: "transparent" }}
-                        href="/"
-                    >
-                        CRM
-                    </a>
-                    <div className="d-flex header-btn">
-                        <div className="dropdown">
-                            <button className="drop-toggle">
-                                <div className="circle">
-                                    <FontAwesomeIcon
-                                        icon={faPlus}
-                                        id="head-plus"
-                                    />
-                                </div>
-                            </button>
-                            <div className="dropdown-menu-1">
-                                <div className="row">
-                                    <div className="col-4">
-                                        <Link
-                                            to="/leads/create/1"
-                                            className="btn head-btn"
-                                        >
-                                            <FontAwesomeIcon
-                                                icon={faFilter}
-                                                className="head-icon icon-left"
-                                            />
-                                            <h6 className="text-muted">
-                                                Leads
-                                            </h6>
-                                        </Link>
-                                    </div>
-                                    <div className="col-4">
-                                        <a
-                                            href="/create/salesperson"
-                                            className="btn head-btn"
-                                        >
-                                            <FontAwesomeIcon
-                                                icon={faUsers}
-                                                className="head-icon"
-                                            />
-                                            <h6 className="text-muted">
-                                                Person
-                                            </h6>
-                                        </a>
-                                    </div>
-                                    <div className="col-4">
-                                        <a
-                                            href="/create/book"
-                                            className="btn head-btn"
-                                        >
-                                            <FontAwesomeIcon
-                                                icon={faBook}
-                                                className="head-icon"
-                                            />
-                                            <h6 className="text-muted">
-                                                Books
-                                            </h6>
-                                        </a>
-                                    </div>
-                                    <div className="col-4">
-                                        <a
-                                            href="/create/publisher"
-                                            className="btn head-btn"
-                                        >
-                                            <FontAwesomeIcon
-                                                icon={faUserTie}
-                                                className="head-icon icon-left"
-                                            />
-                                            <h6 className="text-muted">
-                                                Publisher
-                                            </h6>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+  let {logoutUser, Name } = useContext(AuthContext)
+  return (
+    <div>
+        <nav className="navbar">
+            <div className="container-fluid d-flex">
+                <a className="navbar-brand" style={{ backgroundColor: 'transparent' }} href="/">CRM</a>
+                <div className='d-flex header-btn'>
 
                   <div className="dropdown">
                     <button className="drop-toggle">
@@ -180,9 +104,10 @@ function Header() {
                     </ul>
                   </div>
                 </div>
-            </nav>
-        </div>
-    );
+            </div>
+        </nav>
+    </div>
+  )
 }
 
-export default Header;
+export default Header
